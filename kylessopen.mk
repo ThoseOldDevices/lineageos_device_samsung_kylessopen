47bc3986f984be7e4d06762d762b991d8522015c
andrew264 <andrewsanthosh264@gmail.com>
2019-10-06 15:32:20 +0530

    .

---
+++ kylessopen.mk
@@ -0,0 +1,201 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/samsung/kylessopen/kylessopen-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/kylessopen/overlay
+
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_PACKAGES += \
+    libnetcmdiface \
+    libloc_api-rpc \
+    libqservice
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio_policy.msm7x27a \
+    audio_policy.conf \
+    libaudioutils \
+    audio.primary.msm7x27a \
+    audio.usb.default \
+    libtinyalsa \
+    libdashplayer
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    macloader \
+    hostapd \
+    libnetcmdiface \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    bluetooth-headers \
+    hciconfig \
+    hciattach \
+    btmon \
+    btproxy \
+    bluetoothd-snoop \
+    btmgmt \
+    hcitool \
+    l2ping \
+    libsbc \
+    avinfo \
+    bccmd \
+    haltest \
+    libdbus \
+    audio.sco.default \
+    bluetoothd
+
+# Camera
+PRODUCT_PACKAGES += \
+   camera.msm7x27a
+
+# Snap Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+# Device
+PRODUCT_PACKAGES += \
+    DeviceParts \
+    make_ext4fs \
+    setup_fs \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs \
+    com.android.future.usb.accessory
+
+# IPv6 tethering
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes
+
+# Graphics
+PRODUCT_PACKAGES += \
+    hwcomposer.msm7x27a \
+    libtilerenderer
+    
+# Torch
+PRODUCT_PACKAGES += Torch
+    
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm7x27a
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+    LiveWallpapersPicker \
+    librs_jni
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.msm7x27a
+
+# Recorder
+PRODUCT_PACKAGES += \
+    Recorder
+
+# Video
+PRODUCT_PACKAGES += \
+    libmm-omxcore \
+    libOmxCore \
+    libstagefrighthw
+
+# Display
+PRODUCT_PACKAGES += \
+	libqdutils
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/samsung/kylessopen/ramdisk,root)
+
+# Prebuilt
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/samsung/kylessopen/prebuilt/system,system)
+    
+# Classic Webview
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.webview.provider=classic
+    
+#Optimization for low-memory devices
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.low_ram=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+   media.stagefright.enable-player=true \
+   media.stagefright.enable-meta=false \
+   media.stagefright.enable-scan=true \
+   media.stagefright.enable-http=true \
+   media.stagefright.enable-fma2dp=true \
+   media.stagefright.enable-aac=true \
+   media.stagefright.enable-qcp=true
+
+# Dex2Oat multi-thread
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.dex2oat_thread_count=2
+
+# UI & Performance tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.min.fling_velocity=8000 \
+    ro.min_pointer_dur=8 \
+    windowsmgr.max_events_per_sec=150 \
+    debug.performance.tuning=1
+
+# Low-RAM configs
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bg_apps_limit=10 \
+    config.disable_atlas=true
+
+# Dalvik heap config
+include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
+
+# Texture config.
+include frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk
+
+# Aggressive Trimming
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.use_trim_settings=true
+
+# Hardware features available on this device
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    device/samsung/kylessopen/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
+    device/samsung/kylessopen/recovery/postrecoveryboot.sh:recovery/system/bin/postrecoveryboot.sh
+    
+# Enable repeatable keys in CWM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cwm.enable_key_repeat=true
+
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := lineage_kylessopen
+PRODUCT_DEVICE := kylessopen
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-S7562
